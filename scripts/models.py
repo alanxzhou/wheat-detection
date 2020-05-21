67239e23dd3a9684064a60e148290f8f0bc61a90
Alan <alanzhou93@gmail.com>
2020-05-21 18:08:35 -0400

    moved functions I expect to use later into .py files

---
+++ scripts/models.py
@@ -0,0 +1,162 @@
+import albumentations as A
+import math
+import numpy as np
+import pandas as pd
+import torch
+import torchvision
+
+from albumentations.pytorch.transforms import ToTensor
+from torch.utils.data import DataLoader, Dataset
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+from scripts.utils import Averager, plot_grad_flow, WheatDataset
+
+
+class model_container:
+
+    def __init__(self, data, train_path, num_epochs=5, train_val_split=0.8, model_name='faster_rcnn', optimizer=None, lr_scheduler=None, transforms=None):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.train_path = train_path
+        self.num_epochs = num_epochs
+        self.dataset = data
+        self.train_df = None
+        self.val_df = None
+        self.train_dataset = None
+        self.val_dataset = None
+        self.train_val_split = train_val_split
+        self.transforms = transforms
+        self.load_data()
+        self.train_data_loader = None
+        self.valid_data_loader = None
+        self.set_dataloader()
+        self.num_classes = 2
+        self.model_name = None
+        self.model = None
+        self.select_model(model_name)  # selects model name and model
+        self.model.to(self.device)
+        self. params = [p for p in self.model.parameters() if p.requires_grad]
+        self.optimizer = torch.optim.SGD(self.params, lr=0.005, momentum=0.9, weight_decay=0.0005) \
+            if optimizer is None else optimizer
+        self.lr_scheduler = lr_scheduler
+
+    def load_data(self):
+        train_df = pd.read_csv(self.train_path)
+        train_df['x'] = -1
+        train_df['y'] = -1
+        train_df['w'] = -1
+        train_df['h'] = -1
+        bboxes = train_df['bbox'].copy().apply(lambda x: eval(x))
+        train_df[['x', 'y', 'w', 'h']] = np.stack(bboxes).astype(int)
+
+        image_ids = train_df['image_id'].unique()
+        n_ids = len(image_ids)
+        n_train = round(self.train_val_split * n_ids)
+        train_ids = image_ids[-n_train:]  # I think everything's already been randomized so we don't need to do it again
+        valid_ids = image_ids[:-n_train]
+        valid_df = train_df[train_df['image_id'].isin(valid_ids)]
+        train_df = train_df[train_df['image_id'].isin(train_ids)]
+        self.train_df = train_df
+        self.val_df = valid_df
+        self.train_dataset = WheatDataset(train_df, self.train_path, self.get_train_transform())
+        self.val_dataset = WheatDataset(valid_df, self.train_path, self.get_valid_transform())
+
+    def select_model(self, model_name):
+        self.model_name = model_name
+        if model_name == 'faster_rcnn':
+            model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+            # get number of input features for the classifier
+            in_features = model.roi_heads.box_predictor.cls_score.in_features
+
+            # replace the pre-trained head with a new one
+            model.roi_heads.box_predictor = FastRCNNPredictor(in_features, self.num_classes)
+            weights_model_name = 'fasterrcnn'
+        elif model_name == 'mask_rcnn':
+            model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+            # get number of input features for the classifier
+            in_features = model.roi_heads.box_predictor.cls_score.in_features
+            dim_reduced = 2  # not sure what this should be yet
+
+            model.roi_heads.box_predictor = MaskRCNNPredictor(in_features, dim_reduced, self.num_classes)
+            weights_model_name = 'maskrcnn'
+        else:
+            raise ValueError('Not a valid model name')
+
+    def get_train_transform(self):
+        transforms = [] if self.transforms is None else self.transforms
+        return A.Compose(transforms + [ToTensor()], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+
+    @staticmethod
+    def get_valid_transform():
+        return A.Compose([
+            ToTensor()
+        ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+
+    def set_dataloader(self, train_batch_size=4, val_batch_size=4, shuffle=False, num_workers=0):
+        self.train_data_loader = DataLoader(
+            self.train_dataset,
+            batch_size=train_batch_size,
+            shuffle=shuffle,
+            num_workers=num_workers,
+            collate_fn=collate_fn
+        )
+
+        self.valid_data_loader = DataLoader(
+            self.val_dataset,
+            batch_size=val_batch_size,
+            shuffle=shuffle,
+            num_workers=num_workers,
+            collate_fn=collate_fn
+        )
+
+    def main(self):
+        loss_hist = Averager()
+        loss = []
+        itr = 1
+
+        for epoch in range(self.num_epochs):
+            loss_hist.reset()
+
+            for images, targets, image_ids in self.train_data_loader:
+
+                images = list(image.to(self.device) for image in images)
+                #         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+                targets = [{k: v.long().to(self.device) for k, v in t.items()} for t in targets]
+
+                loss_dict = self.model(images, targets)
+
+                losses = sum(loss for loss in loss_dict.values())
+                loss_value = losses.item()
+
+                loss_hist.send(loss_value)
+                loss.append(loss_value)
+
+                self.optimizer.zero_grad()
+                losses.backward()
+                self.optimizer.step()
+
+                if math.isnan(loss_value):
+                    plot_grad_flow(self.model.named_parameters())
+                    raise ValueError('Loss is nan')
+                if itr % 50 == 0:
+                    print(f"Iteration #{itr} loss: {loss_value}")
+
+                itr += 1
+
+            # update the learning rate
+            if self.lr_scheduler is not None:
+                self.lr_scheduler.step()
+
+            print(f"Epoch #{epoch} loss: {loss_hist.value}")
+        plot_grad_flow(self.model.named_parameters())
+        return loss
+
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+
+if __name__ == '__main__':
+    None
---
+++ scripts/utils.py
@@ -0,0 +1,129 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
+import torch
+
+from torch.utils.data import DataLoader, Dataset
+
+sns.set()
+
+
+class WheatDataset(Dataset):
+    # taken from  https://www.kaggle.com/pestipeti/pytorch-starter-fasterrcnn-train
+
+    def __init__(self, dataframe, image_dir, transforms=None):
+        super().__init__()
+
+        self.image_ids = dataframe['image_id'].unique()
+        self.df = dataframe
+        self.image_dir = image_dir
+        self.transforms = transforms
+
+    def __getitem__(self, index: int):
+        image_id = self.image_ids[index]
+        records = self.df[self.df['image_id'] == image_id]
+
+        image = cv2.imread(f'{self.image_dir}/{image_id}.jpg', cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+
+        boxes = records[['x', 'y', 'w', 'h']].values
+        boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
+        boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
+
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        area = torch.as_tensor(area, dtype=torch.float32)
+
+        # there is only one class
+        labels = torch.ones((records.shape[0],), dtype=torch.int64)
+
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((records.shape[0],), dtype=torch.int64)
+
+        target = {}
+        target['boxes'] = boxes
+        target['labels'] = labels
+        # target['masks'] = None
+        target['image_id'] = torch.tensor([index])
+        target['area'] = area
+        target['iscrowd'] = iscrowd
+
+        if self.transforms:
+            sample = {
+                'image': image,
+                'bboxes': target['boxes'],
+                'labels': labels
+            }
+            sample = self.transforms(**sample)
+            image = sample['image']
+
+            target['boxes'] = torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1, 0)
+
+        return image, target, image_id
+
+    def __len__(self) -> int:
+        return self.image_ids.shape[0]
+
+
+class Averager:
+    def __init__(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+
+    def send(self, value):
+        self.current_total += value
+        self.iterations += 1
+
+    @property
+    def value(self):
+        if self.iterations == 0:
+            return 0
+        else:
+            return 1.0 * self.current_total / self.iterations
+
+    def reset(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+
+
+def show_example(train_df, train_path, image_id='c14c1e300'):
+    image = cv2.imread(os.path.join(train_path, f'{image_id}.jpg'), cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+    image /= 255.0
+
+    bboxes = train_df[train_df.image_id == image_id][['x', 'y', 'w', 'h']]
+
+    fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+
+    for index, row in bboxes.iterrows():
+        cv2.rectangle(image,
+                      (row['x'], row['y']),
+                      (row['x'] + row['w'], row['y'] + row['h']),
+                      (220, 0, 0,), 3)
+
+    ax.set_axis_off()
+    ax.imshow(image)
+
+    plt.show()
+
+
+# adapted from https://discuss.pytorch.org/t/check-gradient-flow-in-network/15063/7
+def plot_grad_flow(named_parameters):
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.show()
